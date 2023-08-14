7e896c8c337dd641f1ff114fc4a889b5af9c212d
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2023-08-14 08:03:33 -0300

    finalização do desafio

--- assets/js/script.Desafio.js
+++ assets/js/script.Desafio.js
@@ -1,53 +1,98 @@
-class Book{
-    constructor(title, autor, pages){
+class Book {
+    constructor(title, author, pages) {
         this.title = title;
-        this.autor = autor;
+        this.author = author;
         this.pages = pages;
     }
-}
 
-class User{
-    constructor(name, age){
-        this.name  = name;
-        this.age = age;
-        this.booksBorrowed = [];
-    }
-
-    getBookBorrewed(books){
-        this.booksBorrowed.push(books);
+    getBookTitle() {
+        return this.book
     }
 }
 
-class Library{
-    constructor(){
-        this.books = [];
-        this.users = [];
+
+
+class User {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+        this.booksBorrowed = []
     }
 
-    addBook(book){
+    getBookBorrowed(book) {
+        this.booksBorrowed.push(book)
+    }
+
+    allMyBooksBorrowed() {
+        console.log(`${this.name} alugou os livros :`)
+        this.booksBorrowed.forEach(book => {
+            if (book.title != undefined) {
+                return console.log(book.title)
+            }else{
+                console.log("Teste");
+            }
+        });
+    }
+
+
+}
+
+
+
+class Library {
+    constructor() {
+        this.users = []
+        this.books = []
+    }
+
+    addBook(book) {
         this.books.push(book);
     }
 
-    addUser(user){
+    addUser(user) {
         this.users.push(user);
     }
+
+    getBooksForUser() {
+        this.users.forEach(user => {
+            if(user.name != undefined) {
+                return user.name + user.allMyBooksBorrowed()
+                
+            }
+        });
+    }
+
 }
 
-const book1 = new Book('Harry Potter', 'J.K Rolling', '300');
-const book2 = new Book('O Lado Feio do Amor', 'Colleen Hoover', '336');
+const book1 = new Book('Harry Potter', 'J. K. Rowling', '300')
+const book2 = new Book('Harry Potter 2', 'J. K. Rowling', '300')
+const book3 = new Book('Harry Potter 3', 'J. K. Rowling', '300')
+const book4 = new Book('Harry Potter 4', 'J. K. Rowling', '300')
+
+const user1 = new User('Rafael Cumpri', '18')
+const user2 = new User('Camila Bersan', '16')
+const library1 = new Library()
 
 
-const user1 = new User('Camila Bersan', '16');
-const user2 = new User('Rafael Cumpri', '18');
+// Usuarios na biblioteca
+library1.addUser(user1)
+library1.addUser(user2)
 
-const library = new Library();
 
-library.addBook(book1);
-library.addUser(user1);
+// Livros na biblioteca
+library1.addBook(book1)
+library1.addBook(book2)
+library1.addBook(book3)
+library1.addBook(book4)
 
-user1.getBookBorrewed(book1);
 
-console.log(book1);
-console.log(user1);
-console.log(library);
+// Usuario 1
+user1.getBookBorrowed(book1)
+user1.getBookBorrowed(book2)
 
+
+// Usuario 2
+user2.getBookBorrowed(book3)
+user2.getBookBorrowed(book4)
+
+console.log(library1.getBooksForUser())
